c2916501fc9398bc1e3ad25f92737fd6bcba849e
manuelbamise <etobi1930@gmail.com>
2025-08-11 15:01:11 +0100

    process: routine save

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
 #include "stdio.h"
 
-int main() { return 0; }
+int main() {
+
+  int age = 400;
+  int year = 2025;
+
+  printf("you are %d years old \n", age);
+  printf("The year is %d", year);
+
+  return 0;
+}
