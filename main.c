80022fa80900e5f997167212c6357757d30dafea
manuelbamise <etobi1930@gmail.com>
2025-08-11 15:21:05 +0100

    process: using foating number with c

--- main.c
+++ main.c
@@ -4,9 +4,14 @@ int main() {
 
   int age = 400;
   int year = 2025;
+  int amount = 5000;
 
   printf("you are %d years old \n", age);
-  printf("The year is %d", year);
+  printf("The year is %d \n", year);
+  printf("The battery costs %d naira \n", amount);
+
+  float height = 5.11;
+  printf("your height is %.4f", height);
 
   return 0;
 }
