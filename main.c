3a4cf50a11550189878828574271b8b05e0ceb1c
manuelbamise <etobi1930@gmail.com>
2025-08-12 20:34:58 +0100

    process: routine save

--- main.c
+++ main.c
@@ -3,13 +3,28 @@
 
 int main() {
 
-  float price1 = 99.34;
-  float price2 = 19.99;
-  float price3 = 25.55;
+  int age;
+  float gpa;
+  char grade;
+  char name[30];
 
-  printf("price: %.2f \n", price1);
-  printf("price: %.2f \n", price2);
-  printf("price: %.2f \n", price3);
+  printf("Enter your age: ");
+  scanf("%d", &age);
+
+  printf("Enter your GPA: ");
+  scanf("%f", &gpa);
+
+  printf("Enter your grade: ");
+  scanf(" %c", &grade);
+
+  getchar();
+  printf("Enter your name: ");
+  fgets(name, sizeof(name), stdin);
+
+  printf("int: %d \n", age);
+  printf("float: %f \n", gpa);
+  printf("char: %c \n", grade);
+  printf("string: %s \n", name);
 
   return 0;
 }
