feadacd9d3bdc9bb55f3aedfbc2bfe609913a5c7
manuelbamise <etobi1930@gmail.com>
2025-08-12 19:51:43 +0100

    process: routine save

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 
 int main() {
 
-  int age = 19;
-  int amount = 50000;
-  int anotherNum = 2500;
+  float price1 = 99.34;
+  float price2 = 19.99;
+  float price3 = 25.55;
+
+  printf("price: %.2f \n", price1);
+  printf("price: %.2f \n", price2);
+  printf("price: %.2f \n", price3);
 
   return 0;
 }
