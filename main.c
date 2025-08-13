9196bb7ee9606a43ca2cd9c8292903d7d3655352
manuelbamise <etobi1930@gmail.com>
2025-08-13 10:20:49 +0100

    process: routine save

--- main.c
+++ main.c
@@ -2,26 +2,4 @@
 #include "stdio.h"
 #include "string.h"
 
-int main() {
-
-  char product[45];
-  float price;
-  int productNumber;
-  float totalAmount;
-
-  printf("What do you want to purchase: ");
-  fgets(product, strlen(product), stdin);
-
-  printf("How many?: ");
-  scanf("%d", &productNumber);
-
-  printf("how much is per product?: ");
-  scanf("%f", &price);
-
-  totalAmount = price * productNumber;
-
-  printf("You purchased %d %s's and the total amount is %f", productNumber,
-         product, totalAmount);
-
-  return 0;
-}
+int main() { return 0; }
