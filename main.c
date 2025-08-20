127a1c3e23f461cd59fbbfdc1fd208dbe2cd1f01
manuelbamise <etobi1930@gmail.com>
2025-08-20 20:21:58 +0100

    process: completed the assignmenr write an equivalent program converting celcius to farenheit

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 int main() {
   /* program to print the farenheit table
    * and the celcius equivalent*/
-  int fahr, celcius;
-  int lower, upper, step;
+  float fahr, celcius;
+  float lower, upper, step;
 
   lower = 0;
   upper = 300;
@@ -13,11 +13,23 @@ int main() {
 
   fahr = lower;
 
+  printf("Table showing farenheit against celcius \n");
+  printf("farenheit \t celcius\n");
   while (fahr <= upper) {
     celcius = 5 * (fahr - 32) / 9;
     // printf("%d \n", celcius);
-    printf("%d \t %d \n", fahr, celcius);
+    printf("%.0f \t %.1f \n", fahr, celcius);
     fahr += step;
   }
+
+  celcius = lower;
+  printf("Table showing celcius to farenheit \n");
+  printf("celcius \t farenheit\n");
+  while (celcius <= upper) {
+    fahr = 32 + (9 * celcius) / 5;
+    printf("%.1f \t %.0f \n", celcius, fahr);
+    celcius += step;
+  }
+
   return 0;
 }
