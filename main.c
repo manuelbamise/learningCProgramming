6f50e4f0022a44c068f80973b73ef4fd0d00066a
manuelbamise <etobi1930@gmail.com>
2025-08-29 10:19:58 +0100

    process: routine save

--- main.c
+++ main.c
@@ -4,21 +4,8 @@
 // #define OUT 0
 
 int main() {
-  int nums[AMOUNTOFCHAR];
-  int input;
 
-  for (int i = 0; i < AMOUNTOFCHAR; ++i) {
-
-    printf("Put in a new number\n");
-    scanf("%d", &input);
-
-    nums[i] = input;
-  }
-
-  printf("the result\n");
-  for (int i = 0; i < AMOUNTOFCHAR; ++i) {
-    printf("%d \n", nums[i]);
-  }
+  int arr[AMOUNTOFCHAR];
 
   return 0;
 }
